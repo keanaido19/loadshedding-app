96a426bd95ebf2be820fbcb355c89c8e8e071ed8
keanaido021 <keanaido021@student.wethinkcode.co.za>
2022-11-10 15:39:59 +0200

    Documented Load Shedding Api

---
+++ common/src/main/java/wethinkcode/responses/error/ErrorObject.java
@@ -0,0 +1,9 @@
+package wethinkcode.responses.error;
+
+import java.util.Map;
+
+public class ErrorObject {
+    public String message;
+    public Map<String, Object> args;
+    public Object value;
+}
